70af3dc7a639762cf52dce3fd5b76f5cd73f9b70
Matheus Wisch <wisch.pg@outlook.com>
2025-07-28 17:27:49 -0300

    Initial commit

---
+++ Balta.Domain/Accounts/Entities/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace Balta.Domain.Accounts.Entities;
+
+public class Student
+{
+    public long Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
